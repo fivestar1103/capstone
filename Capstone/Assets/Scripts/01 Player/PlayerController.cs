3cdcf0433c0eaa34234b03bf25456857138a81b5
dlawotjr1999 <dlawotjr1999@naver.com>
2024-10-10 20:28:04 +0900

    gridmap과 대화창

--- Capstone/Assets/Scripts/01 Player/PlayerController.cs
+++ Capstone/Assets/Scripts/01 Player/PlayerController.cs
@@ -5,72 +5,16 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 
 
-public class PlayerController : MonoBehaviour
+public partial class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    private float moveSpeed = 5f;         // 움직임 속도
-    [SerializeField]
-    private float jumpForce = 10f;        // 점프 세기
-    [SerializeField]
-    private float mouseSensitivity = 100; // 마우스 감도
-
-    private Vector3 moveInput;   // wasd 입력
-    private Vector2 mouseDelta;  // 마우스 회전값
-
-    private Rigidbody rb;        // 2D Rigidbody 사용 (3D라면 Rigidbody 사용)
-
-    private float xRotation = 0f; // X축 회전
-    private CinemachineVirtualCamera playerCam;
-
     [SerializeField]
     private float playerHP = 100;
     public float PlayerHP { get { return playerHP; } }
 
+    private Rigidbody rb;        // 2D Rigidbody 사용 (3D라면 Rigidbody 사용)
+
     // 입력 관련 관련
-    private PlayerInput.PlayerActions PlayerInput { get { return GameManager.PlayerInputs; } }                      // Input System Player 입력
-    public bool JumpPressing { get; private set; }                                                                  // 스페이스바
-
-    // PlayerInput 컴포넌트에서 Move 액션에 대한 입력을 처리
-    public void OnMove(InputAction.CallbackContext context)
-    {
-        moveInput = context.ReadValue<Vector2>();
-    }
-
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        // 점프 입력을 받음; 일단은 무한 점프인데 수정 예정
-        if (context.performed)
-        {
-            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // 위로 점프
-        }
-    }
-
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        mouseDelta = context.ReadValue<Vector2>();
-    }
-
-    private void PlayerMove()
-    {
-        Vector3 moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
-        Vector3 moveVelocity = moveDirection * moveSpeed;
-
-        rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
-    }
-
-    private void PlayerRotate()
-    {
-        float mouseX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
-        float mouseY = mouseDelta.y * mouseSensitivity * Time.deltaTime;
-
-        transform.Rotate(Vector3.up * mouseX);
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // 카메라 상하 회전 각도 제한
-
-        // 카메라의 로컬 회전 설정
-        playerCam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
-    }
+    private PlayerInput.PlayerActions PlayerInput { get { return GameManager.PlayerInputs; } }  // Input System Player 입력                                                          
 
     private void Update()
     {
--- Capstone/Assets/Scripts/01 Player/PlayerMovement.cs
+++ Capstone/Assets/Scripts/01 Player/PlayerMovement.cs
@@ -1,13 +1,65 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public partial class PlayerMovement
+public partial class PlayerController
 {
+    [SerializeField]
+    private float moveSpeed = 5f;         // 움직임 속도
+    [SerializeField]
+    private float jumpForce = 10f;        // 점프 세기
+    [SerializeField]
+    private float mouseSensitivity = 100; // 마우스 감도
 
-  
+    private Vector3 moveInput;   // wasd 입력
+    private Vector2 mouseDelta;  // 마우스 회전값
 
+    private float xRotation = 0f; // X축 회전
+    private CinemachineVirtualCamera playerCam;
 
+    public bool JumpPressing { get; private set; }
 
+    // PlayerInput 컴포넌트에서 Move 액션에 대한 입력을 처리
+    public void OnMove(InputAction.CallbackContext context)
+    {
+        moveInput = context.ReadValue<Vector2>();
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+        // 점프 입력을 받음; 일단은 무한 점프인데 수정 예정
+        if (context.performed)
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse); // 위로 점프
+        }
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseDelta = context.ReadValue<Vector2>();
+    }
+
+    private void PlayerMove()
+    {
+        Vector3 moveDirection = transform.forward * moveInput.y + transform.right * moveInput.x;
+        Vector3 moveVelocity = moveDirection * moveSpeed;
+
+        rb.velocity = new Vector3(moveVelocity.x, rb.velocity.y, moveVelocity.z);
+    }
+
+    private void PlayerRotate()
+    {
+        float mouseX = mouseDelta.x * mouseSensitivity * Time.deltaTime;
+        float mouseY = mouseDelta.y * mouseSensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseX);
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f); // 카메라 상하 회전 각도 제한
+
+        // 카메라의 로컬 회전 설정
+        playerCam.transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+    }
 }
